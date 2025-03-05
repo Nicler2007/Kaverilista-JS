23b388a35502339f3acfb1a70f7ab98801c2acb5
Nico Hede <nico.hede@esedulainen.fi>
2025-03-05 12:17:54 +0200

    muutoksia

--- part 1/main.js
+++ part 1/main.js
@@ -1,26 +1,38 @@
 
 let friends = [];
         
-function addFriend() {
-    let input = document.getElementById("nameInput");
-    let name = input.value.trim();
-    
-    if (name !== "" && friends.length < 10) {
-        friends.push(name);
-        input.value = "";
-        updateList();
-    }
-    if (friends.length === 10) {
-        alert("Olet syöttänyt 10 nimeä!");
-    }
-}
-
-function updateList() {
-    let list = document.getElementById("friend-list");
-    list.innerHTML = "";
-    friends.forEach(friend => {
-        let li = document.createElement("li");
-        li.textContent = friend;
-        list.appendChild(li);
-    });
-}
+        function addFriend() {
+            let input = document.getElementById("nameInput");
+            let name = input.value.trim();
+            
+            if (name !== "") {
+                friends.push(name);
+                input.value = "";
+                updateList();
+            }
+        }
+        
+        function removeFriend(index) {
+            friends.splice(index, 1);
+            updateList();
+        }
+        
+        function sortFriends() {
+            friends.sort();
+            updateList();
+        }
+        
+        function updateList() {
+            let list = document.getElementById("friend-list");
+            list.innerHTML = "";
+            friends.forEach((friend, index) => {
+                let li = document.createElement("li");
+                li.textContent = friend;
+                let removeBtn = document.createElement("button");
+                removeBtn.textContent = "Poista";
+                removeBtn.classList.add("remove-btn");
+                removeBtn.onclick = () => removeFriend(index);
+                li.appendChild(removeBtn);
+                list.appendChild(li);
+            });
+        }
