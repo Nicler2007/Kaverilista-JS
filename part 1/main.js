5e7de572b398a7c63cf66e14c9f30c502e0b8c20
Nico Hede <nico.hede@esedulainen.fi>
2025-03-10 11:20:41 +0200

    tehtävän viimeistely

--- part 1/main.js
+++ part 1/main.js
@@ -1,38 +1,46 @@
-
-let friends = [];
+    
+    let friends = [];
+    
+    function addFriend() {
+        let input = document.getElementById("nameInput");
+        let name = input.value.trim();
         
-        function addFriend() {
-            let input = document.getElementById("nameInput");
-            let name = input.value.trim();
+        if (name === "") {
+            alert("Et voi lisätä tyhjää nimeä!");
+            return;
+        }
+        
+        if (!/^[A-Za-zÅÄÖåäö]+$/.test(name)) {
+            alert("Syötä vain kirjaimia sisältävä nimi! Yritä uudelleen.");
+            return;
+        }
+        
+        friends.push(name);
+        input.value = "";
+        showFriends();
+    }
+    
+    function sortFriends() {
+        friends.sort();
+        showFriends();
+    }
+    
+    function showFriends() {
+        let list = document.getElementById("friend-list");
+        list.innerHTML = "";
+        
+        for (let i = 0; i < friends.length; i++) {
+            let li = document.createElement("li");
+            li.textContent = friends[i];
             
-            if (name !== "") {
-                friends.push(name);
-                input.value = "";
-                updateList();
-            }
+            let removeBtn = document.createElement("button");
+            removeBtn.textContent = "Poista";
+            removeBtn.onclick = function() {
+                friends.splice(i, 1);
+                showFriends();
+            };
+            
+            li.appendChild(removeBtn);
+            list.appendChild(li);
         }
-        
-        function removeFriend(index) {
-            friends.splice(index, 1);
-            updateList();
-        }
-        
-        function sortFriends() {
-            friends.sort();
-            updateList();
-        }
-        
-        function updateList() {
-            let list = document.getElementById("friend-list");
-            list.innerHTML = "";
-            friends.forEach((friend, index) => {
-                let li = document.createElement("li");
-                li.textContent = friend;
-                let removeBtn = document.createElement("button");
-                removeBtn.textContent = "Poista";
-                removeBtn.classList.add("remove-btn");
-                removeBtn.onclick = () => removeFriend(index);
-                li.appendChild(removeBtn);
-                list.appendChild(li);
-            });
-        }
+    }
